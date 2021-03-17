b21c4a89bb69c96f2a7fa15589ed802ae993c00f
adrien-supizet <adrien.supizet@gmail.com>
2021-03-17 19:08:58 +0100

    Use safe mint method and fix mint event

--- contracts/NestedAsset.sol
+++ contracts/NestedAsset.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC721/ERC721Burnable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NestedAsset is ERC721, ERC721Burnable, Ownable  {
-  event NestedCreated(address indexed adress, address indexed owner); // TODO: add number of underlying assets
+  event NestedCreated(uint256 indexed tokenId, address indexed owner);
 
   using Counters for Counters.Counter;
   Counters.Counter private _tokenIds;
@@ -28,12 +28,11 @@ contract NestedAsset is ERC721, ERC721Burnable, Ownable  {
     _tokenIds.increment();
 
     uint256 newNestedId = _tokenIds.current();
-    _mint(msg.sender, newNestedId);
+    _safeMint(msg.sender, newNestedId);
 
-    console.log("Minting an NFT: ", address(this));
     console.log("Owner is: ", owner());
     console.log("TokenId is: ", newNestedId);
-    emit NestedCreated(address(this), owner());
+    emit NestedCreated(tokenId, owner());
 
     return newNestedId;
   }
--- scripts/0_create_factory_and_mint_nft.js
+++ scripts/0_create_factory_and_mint_nft.js
@@ -12,8 +12,8 @@ async function main() {
   // Wait that the contract is deployed
   await nestedFactory.deployed();
 
-  // Set the second address as the fee collector
-  await nestedFactory.setFeeTo(accounts[1].address);
+  // Set the first address as the fee collector
+  await nestedFactory.setFeeTo(accounts[0].address);
 
   // Get the Asset contract to deploy
   const NestedAsset = await hre.ethers.getContractFactory("NestedAsset");
@@ -22,7 +22,7 @@ async function main() {
   const nestedAsset = await NestedAsset.deploy(nestedFactory.address);
 
   // Wait that the contract is deployed
-  await nestedFactory.deployed();
+  await nestedAsset.deployed();
 
   // Mint an NFT, it returns the TokenId
   const nftId = await nestedAsset.mint();
