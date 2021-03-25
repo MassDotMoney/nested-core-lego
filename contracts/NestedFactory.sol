34369f883fe376de203277a7baaa1da13ca0a0f1
Harraken <harraken@gmail.com>
2021-03-25 23:32:31 +0200

    Update NestedFactory.sol

--- contracts/NestedFactory.sol
+++ contracts/NestedFactory.sol
@@ -5,7 +5,11 @@ import "hardhat/console.sol";
 import "./NestedAsset.sol";
 import "./NestedReserve.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
+/*interface IERC20 {
+    function balanceOf(address owner) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transfer(address to, uint256 amount) external returns (bool);
+}
 contract NestedFactory {
     event NestedCreated(uint256 indexed tokenId, address indexed owner);
 
@@ -117,5 +121,41 @@ contract NestedFactory {
             //}
             usersHoldings[tokenId].push(Holding({ token: tokens[i], amount: amounts[i], reserve: reserve }));
         }
+    }
+    function fillQuote(
+
+        // The `sellTokenAddress` field from the API response.
+        address sellToken,
+        // The `buyTokenAddress` field from the API response.
+        address buyToken,
+        // The `allowanceTarget` field from the API response.
+        address spender,
+        // The `to` field from the API response.
+        address payable swapTarget,
+        // The `data` field from the API response.
+        bytes calldata swapCallData
+    )
+        external
+        payable // Must attach ETH equal to the `value` field from the API response.
+    {
+      //require(msg.sender == _recipient, "NestedFactory: FORBIDDEN");
+      console.log("IN FILLQUOTE");
+        // Track our balance of the buyToken to determine how much we've bought.
+        uint256 boughtAmount = buyToken.balanceOf(address(this));
+
+        // Give `spender` an infinite allowance to spend this contract's `sellToken`.
+        // Note that for some tokens (e.g., USDT, KNC), you must first reset any existing
+        // allowance to 0 before being able to update it.
+  //require(sellToken.approve(spender, uint256(-1)));
+        // Call the encoded swap function call on the contract at `swapTarget`,
+        // passing along any ETH attached to this function call to cover protocol fees.
+        (bool success,) = swapTarget.call{value: msg.value}(swapCallData);
+        require(success, 'SWAP_CALL_FAILED');
+        // Refund any unspent protocol fees to the sender.
+        msg.sender.transfer(address(this).balance);
+
+        // Use our current buyToken balance to determine how much we've bought.
+        boughtAmount = buyToken.balanceOf(address(this)) - boughtAmount;
+        //emit BoughtTokens(sellToken, buyToken, boughtAmount);
     }
 }
