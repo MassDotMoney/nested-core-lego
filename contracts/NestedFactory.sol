a57a30e42dc7a4b2d5c32cb08220b1e98aaafa46
Jonathan Gueron <jonathan@exeko.com>
2021-04-19 21:26:27 +0300

    destroyForETH + optimize destroyForERC20

--- contracts/NestedFactory.sol
+++ contracts/NestedFactory.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.3;
 import "hardhat/console.sol";
 import "./NestedAsset.sol";
 import "./NestedReserve.sol";
+import "./interfaces/IWETH.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 contract NestedFactory is ReentrancyGuard {
     event NestedCreated(uint256 indexed tokenId, address indexed owner);
 
+    address public immutable weth;
     address payable public feeTo;
     address public feeToSetter;
     NestedReserve public reserve;
@@ -42,7 +44,7 @@ contract NestedFactory is ReentrancyGuard {
     constructor(address payable _feeToSetter) {
         feeToSetter = _feeToSetter;
         feeTo = _feeToSetter;
-
+        weth = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
         nestedAsset = new NestedAsset();
         // TODO: do this outside of constructor. Think about reserve architecture
         reserve = new NestedReserve();
@@ -56,6 +58,11 @@ contract NestedFactory is ReentrancyGuard {
         require(nestedAsset.ownerOf(tokenId) == msg.sender, "NestedFactory: Only Owner");
         _;
     }
+
+    /*
+    Fallback function 
+    */
+    fallback() external payable {}
 
     /*
    Sets the address receiving the fees
@@ -137,16 +144,13 @@ contract NestedFactory is ReentrancyGuard {
             usersHoldings[tokenId].push(
                 Holding({ token: _tokensToBuy[i], amount: amountBought, reserve: address(reserve) })
             );
-            require(IERC20(_tokensToBuy[i]).transfer(address(reserve), amountBought) == true, "TOKEN_TRANSFER_ERROR");
+            require(IERC20(_tokensToBuy[i]).transfer(address(reserve), amountBought), "TOKEN_TRANSFER_ERROR");
         }
 
         uint256 remainingSellToken =
             sellAmountWithFees - (sellTokenBalanceBeforePurchase - IERC20(_sellToken).balanceOf(address(this)));
         require(remainingSellToken >= fees, "INSUFFICIENT_FUNDS");
-        require(
-            IERC20(_sellToken).transferFrom(address(this), feeTo, remainingSellToken) == true,
-            "FEE_TRANSFER_ERROR"
-        );
+        require(IERC20(_sellToken).transferFrom(address(this), feeTo, remainingSellToken), "FEE_TRANSFER_ERROR");
     }
 
     /*
@@ -175,23 +179,26 @@ contract NestedFactory is ReentrancyGuard {
         uint256 fees = amountToSell / 100;
         require(msg.value >= amountToSell + fees, "INSUFFICIENT_FUNDS");
 
-        uint256 ethBalanceBeforePurchase = address(this).balance;
-
         uint256 tokenId = nestedAsset.mint(msg.sender);
 
+        // we wrap ETH first
+        IWETH(weth).deposit{ value: msg.value }();
+        uint256 wethBalanceBeforePurchase = IERC20(weth).balanceOf(address(this));
+
         for (uint256 i = 0; i < buyCount; i++) {
-            swapFromETH(_sellAmounts[i], _swapTarget, _swapCallData[i]);
+            swapTokens(weth, _swapTarget, _swapCallData[i]);
             uint256 amountBought = IERC20(_tokensToBuy[i]).balanceOf(address(this));
 
             usersHoldings[tokenId].push(
                 Holding({ token: _tokensToBuy[i], amount: amountBought, reserve: address(reserve) })
             );
-            require(IERC20(_tokensToBuy[i]).transfer(address(reserve), amountBought) == true, "TOKEN_TRANSFER_ERROR");
+            require(IERC20(_tokensToBuy[i]).transfer(address(reserve), amountBought), "TOKEN_TRANSFER_ERROR");
         }
 
-        uint256 remainingETH = msg.value - (ethBalanceBeforePurchase - address(this).balance);
-        require(remainingETH >= fees, "INSUFFICIENT_FUNDS");
-        feeTo.transfer(remainingETH);
+        uint256 remainingWETH = msg.value - (wethBalanceBeforePurchase - IERC20(weth).balanceOf(address(this)));
+        require(remainingWETH >= fees, "INSUFFICIENT_FUNDS");
+
+        require(IERC20(weth).transfer(feeTo, remainingWETH), "FEE_TRANSFER_ERROR");
     }
 
     /*
@@ -211,23 +218,7 @@ contract NestedFactory is ReentrancyGuard {
         require(IERC20(_sellToken).approve(_swapTarget, type(uint256).max), "ALLOWANCE_SETTER_ERROR");
 
         // solhint-disable-next-line avoid-low-level-calls
-        (bool success, ) = _swapTarget.call{ value: msg.value }(_swapCallData);
-        require(success, "SWAP_CALL_FAILED");
-    }
-
-    /*
-    Purchase token with ETH
-    @param _sellAmount [uint256] token used to make swaps
-    @param _swapTarget [address] the address of the contract that will swap tokens
-    @param _swapCallData [bytes] call data provided by 0x to fill the quote
-    */
-    function swapFromETH(
-        uint256 _sellAmount,
-        address payable _swapTarget,
-        bytes calldata _swapCallData
-    ) internal {
-        // solhint-disable-next-line avoid-low-level-calls
-        (bool success, ) = _swapTarget.call{ value: _sellAmount }(_swapCallData);
+        (bool success, ) = _swapTarget.call(_swapCallData);
         require(success, "SWAP_CALL_FAILED");
     }
 
@@ -268,6 +259,8 @@ contract NestedFactory is ReentrancyGuard {
         // get Holdings for this token
         Holding[] memory holdings = usersHoldings[_tokenId];
 
+        require(holdings.length == _tokensToSell.length, "MISSING_SELL_ARGS");
+
         // first transfer holdings from reserve to factory
         for (uint256 i = 0; i < holdings.length; i++) {
             NestedReserve(holdings[i].reserve).transfer(address(this), holdings[i].token, holdings[i].amount);
@@ -284,8 +277,51 @@ contract NestedFactory is ReentrancyGuard {
         uint256 amountBought = IERC20(_buyToken).balanceOf(address(this)) - buyTokenInitialBalance;
         uint256 amountFees = amountBought / 100;
         amountBought = amountBought - amountFees;
-        require(IERC20(_buyToken).transfer(feeTo, amountFees) == true, "FEES_TRANSFER_ERROR");
-        require(IERC20(_buyToken).transfer(msg.sender, amountBought) == true, "TOKEN_TRANSFER_ERROR");
+        require(IERC20(_buyToken).transfer(feeTo, amountFees), "FEES_TRANSFER_ERROR");
+        require(IERC20(_buyToken).transfer(msg.sender, amountBought), "TOKEN_TRANSFER_ERROR");
+
+        delete usersHoldings[_tokenId];
+        nestedAsset.burn(msg.sender, _tokenId);
+    }
+
+    /*
+    Burn NFT and Sell all tokens for WETH, unwrap it and then send ETH back to the user
+    @param  _tokenId uint256 NFT token Id
+    @param _swapTarget [address] the address of the contract that will swap tokens
+    @param _tokensToSell [<address>] the list of tokens to sell
+    @param _swapCallData [<bytes>] the list of call data provided by 0x to fill quotes
+    */
+    function destroyForETH(
+        uint256 _tokenId,
+        address payable _swapTarget,
+        address[] calldata _tokensToSell,
+        bytes[] calldata _swapCallData
+    ) external payable onlyOwner(_tokenId) nonReentrant {
+        // get Holdings for this token
+        Holding[] memory holdings = usersHoldings[_tokenId];
+        require(holdings.length == _tokensToSell.length, "MISSING_SELL_ARGS");
+
+        // first transfer holdings from reserve to factory
+        for (uint256 i = 0; i < holdings.length; i++) {
+            NestedReserve(holdings[i].reserve).transfer(address(this), holdings[i].token, holdings[i].amount);
+        }
+
+        uint256 buyTokenInitialBalance = IERC20(weth).balanceOf(address(this));
+
+        // swap tokens
+        for (uint256 i = 0; i < _tokensToSell.length; i++) {
+            swapTokens(_tokensToSell[i], _swapTarget, _swapCallData[i]);
+        }
+
+        // send to user minus fees
+        uint256 amountBought = IERC20(weth).balanceOf(address(this)) - buyTokenInitialBalance;
+        uint256 amountFees = amountBought / 100;
+        amountBought = amountBought - amountFees;
+        require(IERC20(weth).transfer(feeTo, amountFees), "FEES_TRANSFER_ERROR");
+        IWETH(weth).withdraw(amountBought); // NOT WORKING
+
+        (bool success, ) = msg.sender.call{ value: amountBought }("");
+        require(success, "ETH_TRANSFER_ERROR");
 
         delete usersHoldings[_tokenId];
         nestedAsset.burn(msg.sender, _tokenId);
