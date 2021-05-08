e4acd0e11f184b05ef2d1d0ca907d4b499c24a3e
adrien <adrien.supizet@gmail.com>
2021-05-08 21:19:00 +0200

    add solidity optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -50,6 +50,12 @@ export default {
     },
     solidity: {
         version: "0.8.4",
+        settings: {
+            optimizer: {
+                enabled: true,
+                runs: 5000,
+            },
+        },
     },
     gasReporter: {
         coinmarketcap: process.env.COINMARKETCAP_API_KEY,
