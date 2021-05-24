aa4b7dda5b2f12f01fd608bd1747109599a87346
Adrien Morel <hello@adrienmorel.co>
2021-05-24 14:10:39 +0200

    demo: remove code replicating an asset in the claim fees script

--- demo/5_claim_fees.ts
+++ demo/5_claim_fees.ts
@@ -2,11 +2,10 @@ import { ethers, network } from "hardhat"
 
 import { NetworkName } from "./demo-types"
 import addresses from "./addresses.json"
-import { createNFT } from "./create-nft"
 
-async function main(replicateNFT: number = 0) {
+async function main() {
     const env = network.name as NetworkName
-    const [user1, user2] = await ethers.getSigners()
+    const [user1] = await ethers.getSigners()
 
     const NestedFactory = await ethers.getContractFactory("NestedFactory")
     const nestedFactory = await NestedFactory.attach(addresses[env].factory)
@@ -14,17 +13,6 @@ async function main(replicateNFT: number = 0) {
     const FeeSplitter = await ethers.getContractFactory("FeeSplitter")
     const feeSplitterAddress = await nestedFactory.feeTo()
     const feeSplitter = await FeeSplitter.attach(feeSplitterAddress)
-
-    if (!replicateNFT) {
-        await createNFT(user1)
-        const assets = await nestedFactory.tokensOf(user1.address)
-        console.log(`NFT created and has ID ${assets[0]}`)
-        replicateNFT = assets[0]
-
-        await createNFT(user2, replicateNFT)
-        const assetsUser2 = await nestedFactory.tokensOf(user2.address)
-        console.log(`NFT replicated by user 2. It has ID ${assetsUser2[0]}`)
-    }
 
     const user1Balance = await user1.getBalance()
     console.log(`User 1 balance is ${ethers.utils.formatEther(user1Balance)}. Claiming fees...`)
@@ -35,7 +23,7 @@ async function main(replicateNFT: number = 0) {
     console.log(`User 1 new balance is ${ethers.utils.formatEther(user1newBalance)}`)
 }
 
-main(1)
+main()
     .then(() => process.exit(0))
     .catch(err => {
         console.error(err)
