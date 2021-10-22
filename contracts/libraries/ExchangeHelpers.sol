8cd17b7c03290d11b6bf25bbb90598cc423dc916
axxedevv <maxime.brugel@gmail.com>
2021-10-22 16:33:40 +0200

    fix: Red4Sec - NFSC13

--- contracts/libraries/ExchangeHelpers.sol
+++ contracts/libraries/ExchangeHelpers.sol
@@ -41,15 +41,4 @@ library ExchangeHelpers {
             _token.safeApprove(_spender, type(uint256).max);
         }
     }
-
-    function _getRevertMsg(bytes memory _returnData) internal pure returns (string memory) {
-        // If the _res length is less than 68, then the transaction failed silently (without a revert message)
-        if (_returnData.length < 68) return "Transaction reverted silently";
-
-        assembly {
-            // Slice the sighash.
-            _returnData := add(_returnData, 0x04)
-        }
-        return abi.decode(_returnData, (string)); // All that remains is the revert string
-    }
 }
