9383142028d4d53f249b6860490a71c81205e7d0
axxedevv <maxime.brugel@gmail.com>
2021-10-22 16:39:13 +0200

    fix: Red4Sec - NFSC12 (upgrade solidity version)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -56,7 +56,7 @@ const config: HardhatUserConfig = {
         }
     },
     solidity: {
-        version: "0.8.4",
+        version: "0.8.9",
         settings: {
             optimizer: {
                 enabled: true,
