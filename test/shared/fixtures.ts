bc6a3d3ca54120435caa2c99feb4d3eeb84d3cb1
axxedevv <maxime.brugel@gmail.com>
2021-10-22 17:40:28 +0200

    fix: remove ownableOperator

--- test/shared/fixtures.ts
+++ test/shared/fixtures.ts
@@ -12,7 +12,6 @@ import {
     NestedRecords,
     NestedReserve,
     OperatorResolver,
-    OwnableOperator,
     SynthetixOperator,
     TestableOperatorCaller,
     TestableOwnableOperatorCaller,
@@ -35,39 +34,6 @@ export const operatorResolverFixture: Fixture<OperatorResolverFixture> = async (
     const operatorResolver = await operatorResolverFactory.connect(signer).deploy();
 
     return { operatorResolver };
-};
-
-export type OwnableOperatorFixture = { ownableOperator: OwnableOperator };
-
-export const ownableOperatorFixture: Fixture<OwnableOperatorFixture> = async (wallets, provider) => {
-    const signer = new ActorFixture(wallets as Wallet[], provider).ownableOperatorOwner();
-
-    const ownableOperatorFixtureFactory = await ethers.getContractFactory("OwnableOperator");
-    const ownableOperator = await ownableOperatorFixtureFactory.connect(signer).deploy();
-
-    return { ownableOperator };
-};
-
-export type TestableOwnableOperatorCallerFixture = {
-    testableOwnableOperatorCaller: TestableOwnableOperatorCaller;
-    testableOwnedOperator: TestableOwnedOperator;
-};
-
-export const testableOwnableOperatorCallerFixture: Fixture<TestableOwnableOperatorCallerFixture> = async (
-    wallets,
-    provider,
-) => {
-    const signer = new ActorFixture(wallets as Wallet[], provider).ownableOperatorOwner();
-
-    const testableOwnedOperatorFactory = await ethers.getContractFactory("TestableOwnedOperator");
-    const testableOwnedOperator = await testableOwnedOperatorFactory.connect(signer).deploy();
-
-    const testableOwnableOperatorCallerFactory = await ethers.getContractFactory("TestableOwnableOperatorCaller");
-    const testableOwnableOperatorCaller = await testableOwnableOperatorCallerFactory
-        .connect(signer)
-        .deploy(testableOwnedOperator.address);
-
-    return { testableOwnableOperatorCaller, testableOwnedOperator };
 };
 
 export type ZeroExOperatorFixture = {
