7b2c9f61abd4b0b8c99e674e8c43d1b02a479ed1
axxedevv <maxime.brugel@gmail.com>
2021-10-29 15:46:40 +0200

    refactor: Remove old scripts + move addresses json file test: remove timelock test (useless)

--- demo/deploy-erc20.ts
+++
@@ -1,7 +0,0 @@
-import { ethers } from "hardhat"
-import { appendDecimals } from "../test/helpers"
-
-export const deployERC20 = async (name: string, symbol: string) => {
-    const ERC20Factory = await ethers.getContractFactory("MockERC20")
-    return ERC20Factory.deploy(name, symbol, appendDecimals(10000000))
-}
