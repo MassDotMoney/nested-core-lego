aec59e1bbb69a4c1964dda0fb522a2edc403d10b
axxedevv <maxime.brugel@gmail.com>
2021-11-04 11:55:08 +0100

    feat: set ShareHolders script

---
+++ scripts/setShareholders.ts
@@ -0,0 +1,27 @@
+import hre, { ethers } from "hardhat";
+import addresses from "../addresses.json";
+import { BigNumberish, Wallet } from "ethers";
+
+const chainId: string = hre.network.config.chainId.toString();
+const context = JSON.parse(JSON.stringify(addresses));
+
+async function main(): Promise<void> {
+    let accounts: string[] = [];
+    let weigths: BigNumberish[] = [];
+
+    // Add the accounts with the weigths
+    accounts.push(Wallet.createRandom().address);
+    weigths.push(100);
+
+    const feeSplitterFactory = await ethers.getContractFactory("FeeSplitter");
+    const feeSplitter = await feeSplitterFactory.attach(context[chainId].FeeSplitter);
+
+    await feeSplitter.setShareholders(accounts, weigths);
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error: Error) => {
+        console.error(error);
+        process.exit(1);
+    });
