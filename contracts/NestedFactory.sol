724e440672b2ad15c20fba8eec6376b8066c092a
axxedevv <maxime.brugel@gmail.com>
2021-12-02 15:53:19 +0100

    fix: add operatorResolver address check

--- contracts/NestedFactory.sol
+++ contracts/NestedFactory.sol
@@ -45,7 +45,8 @@ contract NestedFactory is INestedFactory, ReentrancyGuard, Ownable, MixinOperato
             address(_nestedAsset) != address(0) &&
                 address(_nestedRecords) != address(0) &&
                 address(_feeSplitter) != address(0) &&
-                address(_weth) != address(0),
+                address(_weth) != address(0) &&
+                _operatorResolver != address(0),
             "NF: INVALID_ADDRESS"
         );
         nestedAsset = _nestedAsset;
