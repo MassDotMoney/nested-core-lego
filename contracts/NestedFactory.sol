67b8881ed95e63bb4fef525d07f042c78020d123
axxe.eth <maxime.brugel@gmail.com>
2022-02-18 20:21:09 +0100

    fix: fixing merge

--- contracts/NestedFactory.sol
+++ contracts/NestedFactory.sol
@@ -458,7 +458,7 @@ contract NestedFactory is INestedFactory, ReentrancyGuard, OwnableProxyDelegatio
         if (success) {
             require(amounts[1] <= _amountToSpend, "NF: OVERSPENT");
             unchecked {
-                _safeTransferWithFees(IERC20(_inputToken), _amountToSpend - amounts[1], _msgSender(), _nftId)
+                _safeTransferWithFees(IERC20(_inputToken), _amountToSpend - amounts[1], _msgSender(), _nftId);
             }
         } else {
             _safeTransferWithFees(IERC20(_inputToken), _amountToSpend, _msgSender(), _nftId);
