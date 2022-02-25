4b727483327b43ea036c776c5d1562a9861de100
axxe.eth <maxime.brugel@gmail.com>
2022-02-25 12:38:07 +0100

    feat: reach nonce script

---
+++ scripts/reachNonce.ts
@@ -0,0 +1,29 @@
+import hre from "hardhat";
+import { getSigners } from "hardhat-deploy-ethers/dist/src/helpers";
+
+async function main(): Promise<void> { 
+    const nonceToReach = 117; // to set
+
+    const signers = await getSigners(hre);
+    const nextNonce = await hre.ethers.provider.getTransactionCount(signers[0].address);
+
+    if (nonceToReach <= nextNonce) {
+        console.log("Nonce already reached");
+        return;
+    }
+
+    for (let i = nextNonce; i < nonceToReach; i++) {
+        let txSent = await signers[0].sendTransaction({
+            to: signers[0].address,
+            value: 0
+        })
+        await txSent.wait();
+    }
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error: Error) => {
+        console.error(error);
+        process.exit(1);
+    });
