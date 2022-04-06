4dc5e1f7e3bc46b1bbcc2c0f4615a34839263ddf
axxe.eth <maxime.brugel@gmail.com>
2022-04-06 11:53:32 +0200

    script: reachNonce strict nonce number

--- scripts/reachNonce.ts
+++ scripts/reachNonce.ts
@@ -2,22 +2,23 @@ import hre from "hardhat";
 import { getSigners } from "hardhat-deploy-ethers/dist/src/helpers";
 
 async function main(): Promise<void> {
-    const nonceToReach = 117; // to set
+    const nonceToReach = 139; // to set
 
     const signers = await getSigners(hre);
     const nextNonce = await hre.ethers.provider.getTransactionCount(signers[0].address);
 
-    if (nonceToReach <= nextNonce) {
+    if (nonceToReach < nextNonce) {
         console.log("Nonce already reached");
         return;
     }
 
-    for (let i = nextNonce; i < nonceToReach; i++) {
+    for (let i = nextNonce; i <= nonceToReach; i++) {
         let txSent = await signers[0].sendTransaction({
             to: signers[0].address,
             value: 0,
         });
         await txSent.wait();
+        console.log("Reach ", i);
     }
 }
 
