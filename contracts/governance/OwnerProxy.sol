5ed3b60aef85dcb8fdacf606a3834c97f1379e0f
axxe.eth <maxime.brugel@gmail.com>
2022-05-17 10:23:09 +0200

    add dsproxy fork ref

--- contracts/governance/OwnerProxy.sol
+++ contracts/governance/OwnerProxy.sol
@@ -11,6 +11,7 @@ contract OwnerProxy is Ownable {
     /// @param _target Address of the "script" to perform a delegatecall
     /// @param _data The bytes calldata (bytes4 selector must be included)
     /// @return response The delegatecall response
+    /// @dev Fork from https://github.com/dapphub/ds-proxy/blob/e17a2526ad5c9877ba925ff25c1119f519b7369b/src/proxy.sol#L53
     function execute(address _target, bytes memory _data) public payable onlyOwner returns (bytes memory response) {
         require(_target != address(0), "OP: INVALID_TARGET");
 
